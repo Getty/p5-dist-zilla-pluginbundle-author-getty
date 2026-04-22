9b4085a57c5c14576dc6369313a4d4396096f82f
Torsten Raudssus <torsten@raudss.us>
2026-04-22 06:34:49 +0200

    increment $VERSION after 0.307 release

--- lib/Pod/Elemental/Transformer/Author/GETTY.pm
+++ lib/Pod/Elemental/Transformer/Author/GETTY.pm
@@ -1,6 +1,6 @@
 package Pod::Elemental::Transformer::Author::GETTY;
 # ABSTRACT: Transform custom POD commands to =head1 and =head2
-our $VERSION = '0.307';
+our $VERSION = '0.308';
 use Moose;
 with 'Pod::Elemental::Transformer';
 
--- lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
+++ lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
@@ -1,6 +1,6 @@
 package Pod::Weaver::PluginBundle::Author::GETTY;
 # ABSTRACT: GETTY's default Pod::Weaver config
-our $VERSION = '0.307';
+our $VERSION = '0.308';
 use strict;
 use warnings;
 
