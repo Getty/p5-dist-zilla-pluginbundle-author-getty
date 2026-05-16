ddd45332879fad5f857c84c4e63f59919de31a89
Torsten Raudssus <torsten@raudss.us>
2026-05-16 03:59:42 +0200

    increment $VERSION after 0.309 release

--- lib/Dist/Zilla/PluginBundle/Author/GETTY.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GETTY.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::PluginBundle::Author::GETTY;
 # ABSTRACT: BeLike::GETTY when you build your dists
-our $VERSION = '0.309';
+our $VERSION = '0.310';
 use Moose;
 use Dist::Zilla;
 with 'Dist::Zilla::Role::PluginBundle::Easy';
