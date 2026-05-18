fa18b7d0ae9c46db673e3a3d1e8d2888819151c5
Torsten Raudssus <torsten@raudss.us>
2026-05-18 21:54:31 +0200

    increment $VERSION after 0.312 release

--- lib/Dist/Zilla/PluginBundle/Author/GETTY/Docker.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GETTY/Docker.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::PluginBundle::Author::GETTY::Docker;
 # ABSTRACT: Docker image subsection for @Author::GETTY
-our $VERSION = '0.312';
+our $VERSION = '0.313';
 use Moose;
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
--- lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
+++ lib/Pod/Weaver/PluginBundle/Author/GETTY.pm
@@ -1,6 +1,6 @@
 package Pod::Weaver::PluginBundle::Author::GETTY;
 # ABSTRACT: GETTY's default Pod::Weaver config
-our $VERSION = '0.312';
+our $VERSION = '0.313';
 use strict;
 use warnings;
 
