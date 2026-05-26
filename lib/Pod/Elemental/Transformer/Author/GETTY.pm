c66e31c418bbfb97faaa2b0a775d5af1b1871825
Torsten Raudssus <torsten@raudss.us>
2026-05-26 17:54:02 +0200

    increment $VERSION after 0.314 release

--- lib/Pod/Elemental/Transformer/Author/GETTY.pm
+++ lib/Pod/Elemental/Transformer/Author/GETTY.pm
@@ -1,6 +1,6 @@
 package Pod::Elemental::Transformer::Author::GETTY;
 # ABSTRACT: Transform custom POD commands to =head1 and =head2
-our $VERSION = '0.314';
+our $VERSION = '0.315';
 use Moose;
 with 'Pod::Elemental::Transformer';
 
