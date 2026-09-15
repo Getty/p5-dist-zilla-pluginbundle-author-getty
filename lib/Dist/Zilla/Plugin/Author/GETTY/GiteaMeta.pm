d0893f77e5ab71871b53f26360a44b63cd458b77
Torsten Raudssus <torsten@raudss.us>
2026-09-15 22:10:18 +0200

    increment $VERSION after 0.322 release

--- lib/Dist/Zilla/Plugin/Author/GETTY/GiteaMeta.pm
+++ lib/Dist/Zilla/Plugin/Author/GETTY/GiteaMeta.pm
@@ -3,7 +3,7 @@ package Dist::Zilla::Plugin::Author::GETTY::GiteaMeta;
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider';
 
-our $VERSION = '0.322';
+our $VERSION = '0.323';
 
 =head1 DESCRIPTION
 
--- lib/Dist/Zilla/PluginBundle/Author/GETTY/Docker.pm
+++ lib/Dist/Zilla/PluginBundle/Author/GETTY/Docker.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::PluginBundle::Author::GETTY::Docker;
 # ABSTRACT: Docker image subsection for @Author::GETTY
-our $VERSION = '0.322';
+our $VERSION = '0.323';
 use Moose;
 with 'Dist::Zilla::Role::PluginBundle::Easy';
 
