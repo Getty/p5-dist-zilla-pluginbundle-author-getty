76cfc48bb4e74b9eb912ff852d65558324641927
Torsten Raudssus <torsten@raudss.us>
2026-09-22 08:47:18 +0200

    increment $VERSION after 0.325 release

--- lib/Dist/Zilla/Plugin/Author/GETTY/GiteaMeta.pm
+++ lib/Dist/Zilla/Plugin/Author/GETTY/GiteaMeta.pm
@@ -3,7 +3,7 @@ package Dist::Zilla::Plugin::Author::GETTY::GiteaMeta;
 use Moose;
 with 'Dist::Zilla::Role::MetaProvider';
 
-our $VERSION = '0.325';
+our $VERSION = '0.326';
 
 =head1 DESCRIPTION
 
